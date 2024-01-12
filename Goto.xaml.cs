c4349e2e010be97c27f9b8b73e6706dacbb2336c
doug24 <17227248+doug24@users.noreply.github.com>
2024-01-11 17:36:00 -0800

    add full screen mode add image fade add goto image index

---
+++ Goto.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace SlideShow;
+
+/// <summary>
+/// Interaction logic for Goto.xaml
+/// </summary>
+public partial class Goto : Window
+{
+    private readonly int maxIndex;
+
+    public Goto(int currentIndex, int maxIndex)
+    {
+        InitializeComponent();
+
+        this.maxIndex = maxIndex;
+
+        label.Content = $"Index number (1 - {maxIndex}):";
+        input.Text = currentIndex.ToString();
+
+        input.Focus();
+        input.SelectAll();
+    }
+
+    public int Index
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(input.Text) &&
+                int.TryParse(input.Text, out int value) &&
+                value > 0 && value <= maxIndex)
+            {
+                return value;
+            }
+            return -1;
+        }
+    }
+
+    private void OKButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+}
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,42 +1,77 @@
-﻿using ImageLibrary;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using NaturalSort.Extension;
 
 namespace SlideShow;
 
 public partial class MainWindow : Window
 {
-    private List<string> images = new();
+    private List<string> images = [];
     private int currentIndex = 0;
+    private bool isTargetImage1FG;
     private readonly DispatcherTimer timer;
+    private readonly DispatcherTimer keepAlive;
+    private readonly DoubleAnimation daIn;
+    private readonly DoubleAnimation daOut;
 
     public MainWindow()
     {
         InitializeComponent();
 
-        timer = new DispatcherTimer
+        daIn = new()
         {
-            Interval = new System.TimeSpan(0, 0, 10)
+            From = 0,
+            To = 1,
+            Duration = new Duration(TimeSpan.FromSeconds(1)),
+        };
+        daOut = new()
+        {
+            From = 1,
+            To = 0,
+            Duration = new Duration(TimeSpan.FromSeconds(1)),
+        };
+        daIn.Freeze();
+        daOut.Freeze();
+
+        timer = new()
+        {
+            Interval = TimeSpan.FromSeconds(6),
         };
         timer.Tick += (s, e) => ShowNextImage();
 
+        keepAlive = new()
+        {
+            Interval = TimeSpan.FromSeconds(120),
+        };
+        keepAlive.Tick += (s, e) => NativeMethods.KeepAwake();
+
         Loaded += (s, e) =>
         {
-            var location = @"C:\Users\jerem\Pictures\Idle Album";
+            var location = @"C:\Users\djper\OneDrive\Pictures\1974 Show";
 
             // Uncomment this section if you want a folder dialog;
             // otherwise, manually set the 'location' above.
-            var dlg = new FolderBrowserDialog { SelectedPath = location };
-            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-               location = dlg.SelectedPath;
-            }
+            //var dlg = new FolderBrowserDialog { SelectedPath = location };
+            //if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //   location = dlg.SelectedPath;
+            //}
 
             images = ImageLocator.GetImagesFromLocation(location);
-            images.Shuffle();
+            images = [.. images.OrderBy(x => x, StringComparison.OrdinalIgnoreCase.WithNaturalSort())];
+            //images.Shuffle();
+            NativeMethods.KeepAwake();
             ShowImage(0);
             timer.Start();
+        };
+
+        Closing += (s, e) =>
+        {
+            NativeMethods.AllowSleep();
         };
     }
 
@@ -57,11 +92,71 @@ public partial class MainWindow : Window
                 ShowPreviousImage();
                 break;
             case Key.Escape:
-                Close();
+                Escape();
+                break;
+            case Key.F11:
+                ToggleFullScreen();
+                break;
+            case Key.G:
+                if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+                {
+                    GoTo();
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    private void GoTo()
+    {
+        bool enabled = timer.IsEnabled;
+        timer.Stop();
+
+        Goto dlg = new(currentIndex + 1, images.Count);
+        if (dlg.ShowDialog() ?? false && dlg.Index != -1)
+        {
+            currentIndex = dlg.Index - 1;
+            ShowImage(currentIndex);
+        }
+
+        if (enabled)
+        {
+            timer.Start();
+        }
+    }
+
+    private void Escape()
+    {
+        if (WindowStyle == WindowStyle.None)
+        {
+            ToggleFullScreen();
+        }
+        else
+        {
+            Close();
+        }
+    }
+
+    private void ToggleFullScreen()
+    {
+        Visibility = Visibility.Collapsed;
+        if (WindowStyle == WindowStyle.None)
+        {
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Maximized;
+            ResizeMode = ResizeMode.CanResize;
+            Topmost = false;
+            Cursor = Cursors.Arrow;
+        }
+        else
+        {
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+            Topmost = true;
+            Cursor = Cursors.None;
+        }
+        Visibility = Visibility.Visible;
     }
 
     private void Pause()
@@ -74,9 +169,30 @@ public partial class MainWindow : Window
 
     private void ShowImage(int index)
     {
-        var image = Image.FromFile(images[index]) as Bitmap;
-        var imageSource = image?.ToWpfBitmap();
-        targetImage.Source = imageSource;
+        var imageSource = new BitmapImage();
+        imageSource.BeginInit();
+        imageSource.UriSource = new Uri(images[index]);
+        imageSource.CacheOption = BitmapCacheOption.OnLoad;
+        imageSource.EndInit();
+        imageSource.Freeze();
+
+        if (index == 0)
+        {
+            targetImage1.Source = imageSource;
+            isTargetImage1FG = true;
+            WindowState = WindowState.Maximized;
+        }
+        else
+        {
+            var fg = isTargetImage1FG ? targetImage2 : targetImage1;
+            var bg = isTargetImage1FG ? targetImage1 : targetImage2;
+            isTargetImage1FG = !isTargetImage1FG;
+
+            fg.Source = imageSource;
+            fg.BeginAnimation(OpacityProperty, daIn);
+            bg.BeginAnimation(OpacityProperty, daOut);
+        }
+        info.Text = $"{Path.GetFileName(images[index])} ({index + 1}/{images.Count})";
     }
 
     private void ShowNextImage()
